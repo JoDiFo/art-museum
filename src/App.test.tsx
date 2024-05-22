487d1822eb6cb2798e742c86a8914829339ecdfd
alexfokin <sasafokin2@gmail.com>
2024-05-22 18:33:16 +0300

    test: configure first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom"
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const mainText = screen.getByText(/App/i);
+  expect(mainText).toBeInTheDocument();
 });
