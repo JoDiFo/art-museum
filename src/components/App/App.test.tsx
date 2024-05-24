a9321f1bd97a2b0c4e8c60606687148375014f13
alexfokin <sasafokin2@gmail.com>
2024-05-24 08:41:39 +0300

    refactor: change folder structure

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { App } from "./App";
+
+test("renders App", () => {
+  render(<App />);
+  const mainText = screen.getByText(/App/i);
+  expect(mainText).toBeInTheDocument();
+});
