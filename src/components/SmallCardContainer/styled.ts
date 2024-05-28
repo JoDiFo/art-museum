7c67708659953677809538cbc6ddfdf8771cfce1
alexfokin <sasafokin2@gmail.com>
2024-05-28 17:38:16 +0300

    feat: add adaptive layout for SmallCardContainer

--- src/components/SmallCardContainer/styled.ts
+++ src/components/SmallCardContainer/styled.ts
@@ -4,4 +4,12 @@ export const StyledWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, minmax(25%, 1fr));
   gap: 16px;
+
+  @media (width <= 1500px) {
+    grid-template-columns: repeat(2, minmax(40%, 1fr));
+  }
+
+  @media (width <= 1000px) {
+    grid-template-columns: repeat(1, minmax(90%, 1fr));
+  }
 `;
