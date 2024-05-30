ea8e5e53790532b99c53a106299869864ebb250d
alexfokin <sasafokin2@gmail.com>
2024-05-30 23:31:21 +0300

    refactor: remove comments

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -23,38 +23,4 @@ describe("Test Header component", () => {
     const favoritesLink = screen.getByTestId("favoritesLink");
     expect(favoritesLink).toBeInTheDocument();
   });
-
-  // it("Not renders home link", () => {
-  //   render(
-  //     <BrowserRouter>
-  //       <Header />
-  //     </BrowserRouter>,
-  //   );
-
-  //   window.location.pathname = "/";
-
-  //   screen.findByTestId("homeLink").then((homeLink) => {
-  //     expect(homeLink).not.toBeInTheDocument();
-  //   });
-  // });
-
-  // it("Renders home link", () => {
-  //   render(
-  //     <BrowserRouter>
-  //       <Header />
-  //     </BrowserRouter>,
-  //   );
-
-  //   jest.mock("useLocation", () => {
-  //     return "/some";
-  //   });
-
-  //   console.log(window.location.pathname);
-
-  //   const homeLink = screen.queryByTestId("homeLink");
-
-  //   if (!homeLink) expect(false).toBe(true);
-
-  //   expect(homeLink).toBeInTheDocument();
-  // });
 });
